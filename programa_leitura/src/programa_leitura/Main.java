03bcd4b5818ac8e111f045fed250266a76d51789
Your Name <you@example.com>
2024-07-26 15:06:12 -0300

    Fix

--- programa_leitura/src/programa_leitura/Main.java
+++ programa_leitura/src/programa_leitura/Main.java
@@ -5,7 +5,7 @@ public static void main (String[]args) {
 	Scanner in = new Scanner(System.in);
 	int idade = 0;
 	double altura = 0,peso = 0, imc;
-	String nome,cidade,estado,pais,gnr;
+	String nome,gnr;
 	
 	
 	System.out.println("Digite o seu nome:");
@@ -23,56 +23,42 @@ public static void main (String[]args) {
 	in.nextLine();
 	
 	System.out.println("Digite seu peso: ");
-	peso = in.nextDouble();
+	peso = in.nextDouble ();
 	in.nextLine();
-	
-	System.out.println("Digite sua cidade: ");
-	cidade = in.nextLine();
-	
-	System.out.println("Digite seu estado: ");
-	estado = in.nextLine();
-	
-	System.out.println("Digite seu pais ");
-	pais = in.nextLine();
 	
 	System.out.println("Aqui estão suas informações:");
 	
 	System.out.println("Nome:"+ nome );
 	
 	if(idade <= 17) {
-		System.out.println(idade +" Voce é menor de idade");
+		System.out.printf("%d Anos, Voce é menor de idade",idade);
 		
 	}else {
-	System.out.println("Idade:"+ idade );
+	System.out.printf("Idade:%d\n", idade );
 	}
 	
-	System.out.println("Sexo:"+ gnr);
+	System.out.printf("Sexo:%s\n",gnr);
 	
-	System.out.println("Altura:"+altura);
+	System.out.printf("Altura:%.2f\n",altura);
 	
-	System.out.println("Peso:"+ peso +"KG" );
+	System.out.printf("Peso:%.2f KG\n",peso );
 	
 	imc = (peso)/(altura*altura);
 	if(imc <= 16.9) {
-	System.out.println("Voce esta com o Índice de Massa Corporal em:"+imc+" Muito abaixo do peso menor que 16,9");
+	System.out.printf("Voce esta com o Índice de Massa Corporal em:%.2f Muito abaixo do peso menor que 16,9\n",imc);
 	}else if(imc <= 18.4){
-		System.out.println("Voce esta com o Índice de Massa Corporal em:"+imc+" Abaixo do peso entre 17 a 18,4");
+		System.out.printf("Voce esta com o Índice de Massa Corporal em:%.2f Abaixo do peso entre 17 a 18,4\n",imc);
 	}else if(imc <= 24.9) {
-		System.out.println("Voce esta com o Índice de Massa Corporal em:"+imc+" Peso Normal entre 18,5 a 24,9");
+		System.out.printf("Voce esta com o Índice de Massa Corporal em:%.2f Peso Normal entre 18,5 a 24,9\n",imc);
 	}else if(imc <= 29.9) {
-		System.out.println("Voce esta com o Índice de Massa Corporal em:"+imc+" Acima do Peso entre 25 a 29,9");
+		System.out.printf("Voce esta com o Índice de Massa Corporal em:%.2f Acima do Peso entre 25 a 29,9\n",imc);
 	}else if(imc <= 34.9) {
-		System.out.println("Voce esta com o Índice de Massa Corporal em:"+imc+" Obesidade Grau I entre 30 a 34,9");
+		System.out.printf("Voce esta com o Índice de Massa Corporal em:%.2f Obesidade Grau I entre 30 a 34,9\n",imc);
 	}else if(imc <= 40) {
-		System.out.println("Voce esta com o Índice de Massa Corporal em:"+imc+" Obesidade Grau II entre 35 a 40");
+		System.out.printf("Voce esta com o Índice de Massa Corporal em:%.2f Obesidade Grau II entre 35 a 40\n",imc);
 	}else if(imc >= 40) {
-		System.out.println("Voce esta com o Índice de Massa Corporal em:"+imc+" Obesidade Grau III  acima de 40");
+		System.out.printf("Voce esta com o Índice de Massa Corporal em:%.2f Obesidade Grau III  acima de 40\n",imc);
 	}
-	System.out.println("Cidade:"+ cidade );
-	
-	System.out.println("Estado:"+ estado );
-	
-	System.out.println("Páis:"+ pais );
 	
 	in.close();
 }
